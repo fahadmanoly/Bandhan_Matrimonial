062ba5b40e71bfdbeb30a2c41c285a09f5d4e4a0
Fahad Manoly <fahadmanoly@gmail.com>
2023-09-14 18:56:47 +0530

    url changed for frontend deployment

--- Frontend/bandhanfront/src/services/axiosInstance.js
+++ Frontend/bandhanfront/src/services/axiosInstance.js
@@ -5,6 +5,7 @@ import { getToken } from './LocalStorageService';
 const {access_token}  = getToken()
     
 const Axiosinstance = axios.create({
+    // baseURL: 'http://127.0.0.1:8000/api/chat/',
     baseURL: 'https://www.manoly.life/api/chat/',
     headers:{
         Authorization: `Bearer ${access_token}`,
